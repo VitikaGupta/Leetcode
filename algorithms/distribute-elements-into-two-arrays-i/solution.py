d0032fe01f0f9f0544ca06b1d160eb3851159ddf
VitikaGupta <guptavitika05@gmail.com>
2026-08-21 01:53:35 +0530

    feat: add Distribute Elements Into Two Arrays I solution

--- algorithms/distribute-elements-into-two-arrays-i/solution.py
+++ algorithms/distribute-elements-into-two-arrays-i/solution.py
@@ -1,4 +1,13 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s=s.split()
-        return len(s[-1])
+    def resultArray(self, nums: List[int]) -> List[int]:
+        arr1=[nums[0]]
+        arr2=[nums[1]]
+        for i in range(2,len(nums)):
+            if arr1[-1]>arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])   
+        res= arr1+arr2
+        return res         
+
+        
