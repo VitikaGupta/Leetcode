9e48a1034b690221a5af825ba46890b95deeb05d
VitikaGupta <guptavitika05@gmail.com>
2026-08-25 17:47:31 +0530

    feat: add Smallest Missing Multiple of K solution

--- algorithms/smallest-missing-multiple-of-k/solution.py
+++ algorithms/smallest-missing-multiple-of-k/solution.py
@@ -1,16 +1,9 @@
 class Solution:
-    def compareVersion(self, version1: str, version2: str) -> int:
-        d1=version1.split('.')
-        d2=version2.split('.')
-        n=max(len(d1),len(d2))
-        for i in range(n):
-            v1=int(d1[i])  if i <len(d1) else 0
-            v2=int(d2[i])  if i <len(d2) else 0
-            if v1>v2:
-                return 1
-            if v2>v1:
-                return -1
-        return 0        
-
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        for i in range(1,len(nums)+2):
+            if i*k not in nums:
+                return i*k
+                break
 
         
