556b4a60ce1005cf47437854c93a69f81b244db7
VitikaGupta <guptavitika05@gmail.com>
2026-08-30 23:41:25 +0530

    feat: add Removing Minimum and Maximum From Array solution

---
+++ algorithms/removing-minimum-and-maximum-from-array/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+         x=0
+         while 4**x<=n:
+           if 4**x ==n :
+             return True 
+           x+=1
+         return False 
+        
+        
+        
