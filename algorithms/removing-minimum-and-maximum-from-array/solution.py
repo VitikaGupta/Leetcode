b7d02fc23bac9bc223a397e4c2b9c10d9047f4ac
VitikaGupta <guptavitika05@gmail.com>
2026-08-30 23:41:28 +0530

    feat: add Removing Minimum and Maximum From Array solution

--- algorithms/removing-minimum-and-maximum-from-array/solution.py
+++ algorithms/removing-minimum-and-maximum-from-array/solution.py
@@ -1,11 +1,14 @@
-class Solution(object):
-    def isPowerOfFour(self, n):
-         x=0
-         while 4**x<=n:
-           if 4**x ==n :
-             return True 
-           x+=1
-         return False 
-        
-        
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        mini=nums.index(min(nums))
+        maxi=nums.index(max(nums))
+        i=min(mini,maxi)
+        j=max(maxi,mini)
+        front = j+1
+        back=len(nums)-i
+        both=(i+1)+(len(nums)-j)
+        a=min(front,back,both)
+        return a
+
+
         
