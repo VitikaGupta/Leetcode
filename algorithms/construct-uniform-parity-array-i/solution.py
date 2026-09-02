28e61f1c67d08d5b8f2b52bcdc1a271115fd19ef
VitikaGupta <guptavitika05@gmail.com>
2026-09-02 08:06:09 +0530

    feat: add Construct Uniform Parity Array I solution

--- algorithms/construct-uniform-parity-array-i/solution.py
+++ algorithms/construct-uniform-parity-array-i/solution.py
@@ -1,14 +1,17 @@
 class Solution:
-    def subtractProductAndSum(self, n: int) -> int:
-        di=0
-        sum=0
-        pro=1
-        while n>0:
-            digit=n%10
-            sum+=digit
-            pro=pro*digit
-            n//=10
+    def uniformArray(self, nums1: list[int]) -> bool:
+        if all(x%2==0 for x in nums1) or all (x%2 for x in nums1):
+            return True
+        else:
+            nums2=[]
+            for i in range(len(nums1)):
+                for j in range(len(nums1)):
+                    if i!=j:
+                        a=nums1[i] - nums1[j]
+                        if a%2!=0:
+                            nums2.append(a)
+                            break
+            return True                
 
-        di=pro-sum
-        return di
 
+        
