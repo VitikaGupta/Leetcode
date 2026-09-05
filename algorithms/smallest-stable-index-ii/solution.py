8a5cbc07cf9554fd2eceb40b6612dc8c27259284
VitikaGupta <guptavitika05@gmail.com>
2026-09-06 02:13:46 +0530

    feat: add Smallest Stable Index I solution

---
+++ algorithms/smallest-stable-index-ii/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+
+        for i in range(len(nums)):
+            a=max(nums[0:i+1])
+            b=min(nums[i:])
+            c=a-b
+            if c<=k:
+                return i
+        return -1        
+
+
+        
