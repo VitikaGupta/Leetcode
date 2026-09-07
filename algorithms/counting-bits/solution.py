9f573020a518024186cc1ba4f784c2294b11ed41
VitikaGupta <guptavitika05@gmail.com>
2026-09-08 00:35:27 +0530

    feat: add Counting Bits solution

--- algorithms/counting-bits/solution.py
+++ algorithms/counting-bits/solution.py
@@ -1,6 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range (len(nums))  :
-            for j in range (i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+class Solution:
+    def countBits(self, n):
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i // 2] + (i % 2)
+
+        return ans
