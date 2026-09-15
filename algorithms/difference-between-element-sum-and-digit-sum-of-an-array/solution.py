8d0228babd1ec4b55fc07c55f088ca4c31edd635
VitikaGupta <guptavitika05@gmail.com>
2026-09-15 18:33:06 +0530

    feat: add Difference Between Element Sum and Digit Sum of an Array solution

--- algorithms/difference-between-element-sum-and-digit-sum-of-an-array/solution.py
+++ algorithms/difference-between-element-sum-and-digit-sum-of-an-array/solution.py
@@ -1,8 +1,18 @@
 class Solution:
-    def scoreOfString(self, s: str) -> int:
-        a=0
-        for i in range(len(s)-1):
-            a+=abs(ord(s[i])-ord(s[i+1]))
+    def differenceOfSum(self, nums: List[int]) -> int:
+        element_sum=0
+        for i in nums:
+            element_sum+=i
+        l=[]
+        for i in nums:
+            for j in str(i):
+                l.append(int(j))
+        digit_sum=0
+        for k in l:
+            digit_sum+=k
+        a=abs(element_sum-digit_sum)    
         return a
+
+
 
         
