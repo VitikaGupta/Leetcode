8ff937afb0f013add2b69dffd3a51f615a8ad8a3
VitikaGupta <guptavitika05@gmail.com>
2026-09-18 02:04:47 +0530

    feat: add Reverse String Prefix solution

--- algorithms/reverse-string-prefix/solution.py
+++ algorithms/reverse-string-prefix/solution.py
@@ -1,12 +1,4 @@
 class Solution:
-    def sumOfMultiples(self, n: int) -> int:
-        l=[]
-        for i in range(1,n+1):
-            if i%3==0 ==0 or i%5==0 or i%7==0:
-                l.append(i)
-        sum=0
-        for i in l:
-            sum+=i
-        return sum            
-
-        
+    def reversePrefix(self, s: str, k: int) -> str:
+        s=s[:k][::-1]+ s[k:]
+        return s
