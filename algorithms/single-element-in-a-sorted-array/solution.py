e9e4bbcf17776db2a4a7afef3b768078d07dba2f
VitikaGupta <guptavitika05@gmail.com>
2026-09-18 21:40:34 +0530

    feat: add Single Element in a Sorted Array solution

--- algorithms/single-element-in-a-sorted-array/solution.py
+++ algorithms/single-element-in-a-sorted-array/solution.py
@@ -1,26 +1,13 @@
 class Solution:
-    import string 
-    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
-        
-        for i in string.punctuation:
-            paragraph=paragraph.replace(i," ")
-        paragraph=paragraph.lower()
-        paragraph=paragraph.split()
-
-        
+    def singleNonDuplicate(self, nums: List[int]) -> int:
         d={}
-        for ch in  paragraph :
+        for ch in nums:
             if ch in d:
                 d[ch]+=1
             else:
                 d[ch]=1
-        max_c=0
-        a=""        
-        for key,value in d.items():
-            if key not in banned:
-                if value > max_c:
-                    max_c = value
-                    a=key
-        return a            
+        for key ,value in d.items():
+            if value==1:
+                return key
 
         
