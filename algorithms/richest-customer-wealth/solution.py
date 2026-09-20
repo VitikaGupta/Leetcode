b87241513fe4c80af17f08ab00b840fd364acb74
VitikaGupta <guptavitika05@gmail.com>
2026-09-20 22:32:36 +0530

    feat: add Richest Customer Wealth solution

--- algorithms/richest-customer-wealth/solution.py
+++ algorithms/richest-customer-wealth/solution.py
@@ -1,36 +1,15 @@
 class Solution:
-    def reverseDegree(self, s: str) -> int:
-        sum=0
-        i=1
-        d={'a':26,
-        'b':25,
-        'c':24,
-        'd':23,
-        'e':22,
-        'f':21,
-        'g':20,
-        'h':19,
-        'i':18,
-        'j':17,
-        'k':16,
-        'l':15,
-        'm':14,
-        'n':13,
-        'o':12,
-        'p':11,
-        'q':10,
-        'r':9,
-        's':8,
-        't':7,
-        'u':6,
-        'v':5,
-        'w':4,
-        'x':3,
-        'y':2,
-        'z':1}
-        for j in s:
-            
-            sum+=d[j] *i
-            i+=1
-        return sum
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        l=[]
+        for ch in accounts:
+            sum=0
+            for i in ch:
+                sum+=i
+            l.append(sum)
+        max=0
+        for i in l:
+            if i>max:
+                max=i
+        return max      
                 
+       
