59321913a4017125fe9ec5d7153873177292e414
VitikaGupta <guptavitika05@gmail.com>
2026-09-20 19:55:43 +0530

    feat: add Reverse Degree of a String solution

--- algorithms/reverse-degree-of-a-string/solution.py
+++ algorithms/reverse-degree-of-a-string/solution.py
@@ -1,13 +1,36 @@
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        d={}
-        for ch in nums:
-            if ch in d:
-                d[ch]+=1
-            else:
-                d[ch]=1
-        for key ,value in d.items():
-            if value==1:
-                return key
-
-        
+    def reverseDegree(self, s: str) -> int:
+        sum=0
+        i=1
+        d={'a':26,
+        'b':25,
+        'c':24,
+        'd':23,
+        'e':22,
+        'f':21,
+        'g':20,
+        'h':19,
+        'i':18,
+        'j':17,
+        'k':16,
+        'l':15,
+        'm':14,
+        'n':13,
+        'o':12,
+        'p':11,
+        'q':10,
+        'r':9,
+        's':8,
+        't':7,
+        'u':6,
+        'v':5,
+        'w':4,
+        'x':3,
+        'y':2,
+        'z':1}
+        for j in s:
+            
+            sum+=d[j] *i
+            i+=1
+        return sum
+                
