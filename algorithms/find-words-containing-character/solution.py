fd891f571413075838e7e728173c73c23bf3d655
VitikaGupta <guptavitika05@gmail.com>
2026-09-22 22:19:29 +0530

    feat: add Find Words Containing Character solution

--- algorithms/find-words-containing-character/solution.py
+++ algorithms/find-words-containing-character/solution.py
@@ -1,21 +1,8 @@
 class Solution:
-    def isBalanced(self, num: str) -> bool:
-        l1=[]
-        l2=[]
-        for i in range(len(num)):
-            if i%2==0:
-                l1.append(num[i])
-            else:
-                l2.append(num[i])
-        sum1=0
-        for i in l1:
-            sum1+=int(i)
-        sum2=0
-        for i in l2:
-            sum2+=int(i)
-        if sum1==sum2:
-            return True
-        else:
-            return False                     
-
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        l=[]
+        for i in range(len(words)):
+            if x in words[i]:
+                l.append(i)
+        return l
         
