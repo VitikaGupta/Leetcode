6edc14b2149f6813d297151d3d44bd94fd4f42de
VitikaGupta <guptavitika05@gmail.com>
2026-09-26 02:14:33 +0530

    feat: add Reverse Prefix of Word solution

--- algorithms/reverse-prefix-of-word/solution.py
+++ algorithms/reverse-prefix-of-word/solution.py
@@ -1,17 +1,10 @@
 class Solution:
-    def smallestIndex(self, nums: List[int]) -> int:
-        l=[]
-        for i in nums:
-            sum=0
-            while i>0:
-                sum+=i%10
-                i//=10
-            l.append(sum)
-
-        a=-1
-        for i in range(len(l)):
-            if i==l[i]:
-                a=i
+    def reversePrefix(self, word: str, ch: str) -> str:
+        a=word
+        for i in range(len(word)):
+            if word[i]==ch:
+                a=word[:i+1][::-1]+word[i+1:]
                 break
-        return a           
+        return a
+           
         
